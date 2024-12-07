7b59921cbe9e1359e3ccfd21692c037d60aaedd8
Bear2Be2 <daf@tbm.photography>
2024-12-07 15:17:11 -0600

    make the blinky actually blinky

--- 1_blinky/main.cpp
+++ 1_blinky/main.cpp
@@ -1,28 +1,16 @@
-#include "estuary.h"
+#include "earth.h"
 #include "daisysp.h"
 
-ccam::hw::Estuary hw;
-
-static void AudioCallback(daisy::AudioHandle::InputBuffer in, 
-            daisy::AudioHandle::OutputBuffer out, 
-            size_t size) {
-    hw.ProcessAllControls();
-
-    for(uint8_t i = 0; i < hw.knobs.size(); i++) {
-        hw.leds[i].Set(hw.knobs[i]->Value());
-    }
-
-    hw.PostProcess();
-}
+ccam::hw::Earth hw;
 
 int main(void)
 {
     hw.Init();
-    hw.StartAudio(AudioCallback);
-
-    hw.som.StartLog(false);
-
+    bool ledOn = false;
     while(1) {
+        ledOn = !ledOn;
+        hw.leds[0].Set(ledOn ? 0.0f : 1.0f);
+        hw.PostProcess();
         daisy::System::Delay(1000);
     }
 }
