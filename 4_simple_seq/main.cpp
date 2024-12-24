846878efe1626402e221ddcac3f3e6febd8dbe1b
Garnet Grimm <garnetgrimm720@gmail.com>
2024-12-24 12:04:01 -0600

    fix seq

--- 4_simple_seq/main.cpp
+++ 4_simple_seq/main.cpp
@@ -5,15 +5,45 @@ ccam::hw::Estuary hw;
 
 daisysp::Metro clock;
 uint8_t step_num = 0;
-float step_val = 0.0f;
 // HZ = (BPM / 60)
 float clk_freq = 240.0f / 60.0f;
 
 size_t ticks_since_high = 0;
 float inverse_sample_rate = 0.0f;
 
-float vco_freq = 220.0f;
-daisysp::Oscillator vco;
+std::array<daisysp::Oscillator, 2> vcos;
+
+uint8_t SeqLen() {
+    if (hw.switches[0].Read() == daisy::Switch3::POS_CENTER) {
+        return 4;
+    }
+    return 8;
+}
+
+void StepSeq() {
+    static uint8_t seq_step = 1;
+    uint8_t seq_len = SeqLen();
+
+    switch(hw.switches[1].Read() ) {
+        case daisy::Switch3::POS_LEFT:
+            seq_step = 1;
+            break;
+        case daisy::Switch3::POS_CENTER:
+            if (step_num+1 == seq_len) {
+                seq_step = -1;
+            } else if (step_num == 0) {
+                seq_step = 1;
+            }
+            break;
+        case daisy::Switch3::POS_RIGHT:
+            seq_step = -1;
+            break;
+        default:
+            break;
+    }
+
+    step_num = (step_num + seq_step) % seq_len;
+}
 
 static void AudioCallback(daisy::AudioHandle::InputBuffer in,
             daisy::AudioHandle::OutputBuffer out, 
@@ -31,20 +61,29 @@ static void AudioCallback(daisy::AudioHandle::InputBuffer in,
         }
 
         if (clock.Process()) {
-            step_num = (step_num + 1) % hw.knobs.size();
-            step_val = hw.knobs[step_num]->Value();
-            hw.som.WriteCvOut(0, step_val);
+            StepSeq();
+
+            auto write_out = [&](uint8_t channel, uint8_t step) {
+                float value = hw.knobs[step]->Value();
+                hw.som.WriteCvOut(channel, value*5.0f);
+                vcos[channel].SetFreq(daisysp::fmap(value, 44.0f, 440.0f));
+            };
+            
+            write_out(0, step_num);
+            write_out(1, SeqLen() == 4 ? (7 - step_num) : step_num);
         }
         
-        vco_freq = daisysp::fmap(step_val, 44.0f, 440.0f);
-        vco.SetFreq(vco_freq);
-        float voice = vco.Process();
-        OUT_L[i] = voice;
-        OUT_R[i] = voice;
+        OUT_L[i] = vcos[0].Process();
+        OUT_R[i] = vcos[1].Process();
     }
 
     for (uint8_t i = 0; i < hw.leds.size(); i++) {
-        hw.leds[i].Set(i == step_num ? 0.0f : 1.0f);
+        bool enabled = (i == step_num);
+        if (SeqLen() == 4) {
+            enabled |= (i == (7 - step_num));
+        }
+        
+        hw.leds[i].Set(enabled ? 0.0f : 1.0f);
     }
 
     hw.PostProcess();
@@ -59,10 +98,13 @@ int main(void)
 
     clock.Init(clk_freq, hw.som.AudioSampleRate());
 
-    vco.Init(hw.som.AudioSampleRate());
-    vco.SetAmp(1.0f);
+    for (auto& vco : vcos) {
+        vco.Init(hw.som.AudioSampleRate());
+        vco.SetAmp(1.0f);
+    }
+
     while(1) {
-        hw.som.PrintLine("Step %d freq %f tempo %f", step_num+1, vco_freq, clk_freq);
+        hw.som.PrintLine("Step %d tempo %f", step_num+1, clk_freq);
         daisy::System::Delay(100);
     }
 }
