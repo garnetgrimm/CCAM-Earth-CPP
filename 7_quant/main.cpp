fd5dbb6815e42180c09f9d4c3978348803985216
Garnet Grimm <garnetgrimm720@gmail.com>
2025-01-12 15:29:12 -0600

    quant

---
+++ 7_quant/main.cpp
@@ -0,0 +1,16 @@
+#include "earth.h"
+#include "daisysp.h"
+
+ccam::hw::Earth hw;
+
+int main(void)
+{
+    hw.Init();
+    bool ledOn = false;
+    while(1) {
+        ledOn = !ledOn;
+        hw.leds[0].Set(ledOn ? 0.0f : 1.0f);
+        hw.PostProcess();
+        daisy::System::Delay(1000);
+    }
+}
